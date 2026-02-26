2f41dfbdbc84f34eadcbfda84f5c585f7db62ea1
Tyler Leslie <tylerleslie22@augustana.edu>
2026-02-26 13:06:13 -0600

    Added firebase.js file, updated index.html, index.js, and style.css files

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZWz5_7uClc6rCTh9_9ZBK5DhgkPcazUs",
+  authDomain: "heartbeatapp-c811c.firebaseapp.com",
+  projectId: "heartbeatapp-c811c",
+  storageBucket: "heartbeatapp-c811c.firebasestorage.app",
+  messagingSenderId: "415940987331",
+  appId: "1:415940987331:web:c0b1cd8fdf7ac738bbd576"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
